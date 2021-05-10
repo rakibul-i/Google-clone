953929c45c6764ae5fe6825f20e70d4a3b9b3344
Mohammed Rakib <mohammedrakib81336@gmail.com>
2021-05-10 18:21:11 +0600

    test commit

---
+++ src/components/Home/Home.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './home.css';
+import AppsIcon from '@material-ui/icons/Apps';
+import {Avatar} from '@material-ui/core';
+import Search from '../Search/Search';
+
+const Home = () => {
+    return (
+        <div className="home">
+            <div className="home__header">
+
+                <div className="home__headerLeft">
+                    <Link to ="/about">About</Link>
+                    <Link to="/store">Store</Link>
+                </div>
+
+                <div className="home__headerRight">
+                <Link to ="/gmail">Gmail</Link>
+                <Link to ="/images">Images</Link>
+                <AppsIcon/>
+                <Avatar/>
+                </div>
+
+            </div>
+            <div className="home__body">
+
+            <img src="http://assets.stickpng.com/images/580b57fcd9996e24bc43c51f.png" alt="" />
+            <div className="home__inputContainer">
+                <Search hideButtons />
+            </div>
+            </div>
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/components/Keys.js
@@ -0,0 +1,3 @@
+export const API_KEY = 'AIzaSyBorftlnYLeyA4-Ze5RZ8uUPv29GX28LSM';
+
+export default API_KEY;
